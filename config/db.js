645d68715dda95bebcad7c75a0a4d7d4d09cb74f
Gabriel Coelho <gabriellvs60@gmail.com>
2019-09-18 09:26:41 -0300

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,7 @@
+ module.exports = {
+     url : "mongodb://astolfo:1234@cluster0-shard-00-00-i9cmz.mongodb.net:27017,cluster0-shard-00-01-i9cmz.mongodb.net:27017,cluster0-shard-00-02-i9cmz.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+ }
+
+// module.exports = {
+// 	url : "mongodb://beaufcc:password@ds125146.mlab.com:25146/notable"
+// }
